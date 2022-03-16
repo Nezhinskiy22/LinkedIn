435d0e339dec01f2a1f20bf522a08a7c3d9e1077
Nezhinskiy22 <Nezhinskiy22@gmail.com>
2022-03-16 13:43:52 +0200

    header added

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import React from "react";
+
+const Home = (props) => {
+  return <div>Home</div>;
+};
+
+const Container = styled.div``;
+export default Home;

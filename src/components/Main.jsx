a551558870802925dedc4afb9061882bd3e53817
Nezhinskiy22 <Nezhinskiy22@gmail.com>
2022-03-16 21:22:01 +0200

    sections

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import React from "react";
+
+const Main = (props) => {
+  return <Container>Main</Container>;
+};
+
+export const Container = styled.div``;
+export default Main;
---
+++ src/components/RightSide.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import React from "react";
+
+const RightSide = (props) => {
+  return <Container>RightSide</Container>;
+};
+
+export const Container = styled.div``;
+export default RightSide;
